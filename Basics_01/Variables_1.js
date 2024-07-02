642a789e891ba4b061fdbd7aa2efd0cb47b90daa
Tanushree <122681103+tanu017@users.noreply.github.com>
2024-07-02 12:03:50 +0000

    container config done

---
+++ Basics_01/Variables_1.js
@@ -0,0 +1,18 @@
+const accountId= 492003
+let accountMail= "tanu@gmail.com"
+var accountPassword= "17174444"
+accountCity = "Bangalore"
+let accountState
+
+// accountId = 174
+
+accountMail= "titi@gmail.com"
+accountPassword= "1717"
+accountCity= "Shivamogga"
+
+console.log(accountId);
+console.log(accountMail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+console.table([accountId, accountMail, accountPassword, accountCity])
