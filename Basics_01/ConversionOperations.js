0be02ae0816b2c9cc633614410d6243a2a5228da
Tanushree <122681103+tanu017@users.noreply.github.com>
2024-07-02 12:39:31 +0000

    Conversion of datatypes

---
+++ Basics_01/ConversionOperations.js
@@ -0,0 +1,9 @@
+let score = false
+console.log(typeof score);
+let tcscore = Number(score)
+console.log(typeof tcscore);
+console.log(tcscore);
+let IsLoggedIn= "Tanu"
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(typeof BooleanIsLoggedIn);
+console.log(BooleanIsLoggedIn);
